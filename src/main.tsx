b6961b421f28e0ec02e4026bfee400debafa58f3
Markus T <markus.talka@gmail.com>
2025-08-25 21:08:13 +0300

    Remove missing references, unignore dist for static site distribution

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-import './index.css'
-// import Home from './Home.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <Home />
-//   </StrictMode>,
-// )
+import "./index.css";
 
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
-  {/* prettier-ignore */}
+{/* prettier-ignore */}
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
