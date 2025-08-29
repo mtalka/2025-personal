dfcd373839f77703c7c321408123a3f2da0c275f
Markus T <markus.talka@gmail.com>
2025-08-29 13:14:28 +0300

    About page update

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -7,9 +7,9 @@ export const Route = createFileRoute("/about")({
 function About() {
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex flex-col gap-4 font-light tracking-wide text-justify">
+      <div className="flex flex-col gap-4 font-light tracking-wide">
         <p>
-          Welcome to my humble website. My name is Markus Talka. I am web developer and
+          Welcome to my humble website. I am web developer and
           software industry professional, residing in Helsinki, Finland. My main
           interests are in front-end development, and user experience, but I
           also enjoy working with back-end technologies, platform development,
